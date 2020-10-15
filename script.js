5d8581b02316615b412797b65aed4b91b8b0b36d
anupamroy8 <anupamroy8@gmail.com>
2020-10-15 23:12:47 +0530

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("test");
+var btn = $(".back-to-top");
+console.log(btn);
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
